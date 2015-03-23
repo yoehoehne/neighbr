4c4869bfe8d5a2a63704d36729bd9ea9dfa70550
Joe Hoehne <yoehoehne@gmail.com>
2015-03-23 06:36:53 -0600

    added basic routes

--- server.js
+++ server.js
@@ -1,15 +1,71 @@
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 var express = require('express')
-var app = express()
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var app = express();
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var dbURL = "mongodb://localhost/neighbr";
 
 var server = app.listen(8080, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port)
+    console.log('Example app listening at http://%s:%s', host, port)
 
+});
+
+/***** Routes *******************/
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+app.use('/', express.static('./html', {
+    maxAge: 60 * 60 * 1000
+}));
+
+app.post('/api/newThread', function (req, res) {
+    var thread;
+    thread = req.body;
+    addThread(thread, res);
+});
+
+app.post('/api/getThreads', function(req,res){
+    var location = req.body;
+
+});
+
+app.get('/api/getThreads', function(req,res){
+    MongoClient.connect(dbURL, function(err,db){
+        db.collection('threads').find(function(err,values){
+            res.writeHead(200);
+            res.end(values)
+        })
+    })
 })
+
+
+
+/*********** Other Functions ********************/
+
+var addThread = function(obj, res)
+{
+    addToDB('threads', obj, res);
+}
+
+
+var addToDB = function (collection, obj, res) {
+    MongoClient.connect(dbURL, function (err, db) {
+        if (err) { throw err; }
+
+        db.collection(collection).insert(obj, function (err, records) {
+            if (err) { throw err; }
+            console.log("added "+obj);
+            res.writeHead(200);
+            res.end();
+        });
+    });
+}
