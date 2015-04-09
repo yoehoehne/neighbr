aea1675993570152442a751c11514f6f8b052aad
sdaltonb <ec2-user@ip-172-31-26-146.us-west-2.compute.internal>
2015-04-09 04:42:07 +0000

    Restructured files and such to mock Angular lab. Can roll back if needed.

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var GPSCoordinate = mongoose.model('GPSCoordinate');
-var Thread = mongoose.model('Thread');
-var User = mongoose.model('User');
 
-/******* Routes ************/
-/*
- * Get all threads within a given Location
- */
-router.get('/threads/:location', function(req, res) {
-    var latitude = req.params[0];
-    var longitude = req.params[1];    
-     //Thread.find(function(err, 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-/**
- * Adds a new thread
- */]
-router.post('/threads', function(req, res, next) {
-    var thread = new Thread(req.params[0], new Date().toJSON(), req.params[1], req.params[2], []);
-    thread.save(function(err, thread) {
-        if (err) {
-            return next(err);
-        }
-        res.json(thread);
-    });
-});
-
-/*
- * Get all threads within a given Location
- */
-router.get('/api/threads/', function(req, res) {
-    var radius = req.params[0];
-    var latitude = req.params[1];
-    var longitude = req.params[2];    
-
-    mongoWrapper.readNearbyThreads(radius, latitude, longitude, null);
-});
-
-router.get('/', function(req, res) {
-    res.sendFile(_dirname + '/../html/index.html')
-});
-
-/**
- * Adds a new thread
- */
-router.post('/api/thread', function(req, res, next) {
-    var thread = new Thread(req.params[0], new Date().toJSON(), req.params[1], req.params[2], []);
-    mongoWrapper.createThread(thread);  
-});
-
-/**
- * Gets all threads in the database
- */
-router.get('/threads', function(req, res) {
-});
+module.exports = router;
